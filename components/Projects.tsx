fb603b2b2a4761722d6e3a9c0d391cddd22c1f4f
DC-STEEL <mirajkar101@gmail.com>
2026-05-31 13:53:20 +0530

    Remove unused lucide imports

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
-import { ExternalLink, Github } from 'lucide-react'
 
 interface Project {
   id: number
